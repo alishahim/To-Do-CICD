bbf1a40f19f396cbbf5af0c184e7967ca159ffc9
Aida Yrysbekova <aidiko20@gmail.com>
2025-01-23 00:57:03 -0500

    fixed path

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "Deploying React app to EC2 instance..."
 
 sudo rm -rf /var/www/html/*
-sudo cp -r /home/ubuntu/react-app/* /var/www/html/
+sudo cp -r /home/ubuntu/react-app/build/* /var/www/html/
 sudo systemctl restart nginx
 
 echo "Deployment complete!"
